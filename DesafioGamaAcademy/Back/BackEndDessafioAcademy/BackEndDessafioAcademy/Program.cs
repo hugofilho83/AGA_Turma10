4e761ea12bed10aeb7b59509d98f9113695513c7
hugofilho83 <hugotavares83@gmail.com>
2020-10-14 08:36:16 -0300

    udanças na estrutura do projeto

--- DesafioGamaAcademy/Back/BackEndDessafioAcademy/BackEndDessafioAcademy/Program.cs
+++ DesafioGamaAcademy/Back/BackEndDessafioAcademy/BackEndDessafioAcademy/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace DesafioGamaAcademy
+namespace BackEndDessafioAcademy
 {
     public class Program
     {
