e8db0121a5ba3a2bd9a462686b2eaaf7efd442ac
Alex Souza <alexsouza_kb@outlook.com>
2020-10-16 02:23:12 -0300

    feature/281 - Criação da classes Cliente e Endereço, Migrations e a Camada de serviços.

---
+++ DesafioGamaAcademy/Back/BackEndDessafioAcademy/BackEndDessafioAcademy/Controllers/ClienteController.cs
@@ -0,0 +1,35 @@
+﻿using BackEndDessafioAcademy.Models;
+using BackEndDessafioAcademy.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BackEndDessafioAcademy.Controllers
+{
+    [Route("/Cliente")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "OK";
+        }
+
+        [HttpPost]
+        //[Route("/")]
+        public string Create([FromBody] Cliente cliente)
+        {
+            try
+            {
+                new Service().CadastrarCliente(cliente);
+
+                return "Cadastro efetuado com sucesso.";
+            }
+            catch (Exception exception)
+            {
+                return $"Error: {exception.Message}";
+            }
+        }
+    }
+}
---
+++ DesafioGamaAcademy/Back/BackEndDessafioAcademy/BackEndDessafioAcademy/Models/Endereco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndDessafioAcademy.Models
+{
+    [Table("Enderecos")]
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Pais { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Cep { get; set; }
+        public string Rua { get; set; }
+
+    }
+}
